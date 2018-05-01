928bd65407eca6bdd4fe336e15c6549fdd0d59f7
miguel <miguelsorinpro@gmail.com>
2018-05-01 20:03:34 +0200

    nouveau gossip methode créer

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,5 +1,15 @@
 class GossipsController < ApplicationController
-		def new 
+	
+	def new 
+		@gossip = Gossip.new 
+	end 
+	
+	def create
+	gossip_params = params.require(:gossip).permit(:anonymous_author, :content)
+
+	gossip = Gossip.create(gossip_params) 
+	redirect_to gossips_path
+
 	end 
 	def index
 		@gossips = Gossip.all
@@ -14,9 +24,14 @@ class GossipsController < ApplicationController
 		@gossip = Gossip.find(params[:id])
 
 	end 
+
 	def update 
 		@gossip = Gossip.find(params[:id])
 		gossip_params = params.require(:gossip).permit(:content)
-		@gossip.update(gossip_params)		
+		@gossip.update(gossip_params)
+		redirect_to gossips_path	
 	end 
+
+
+
 end
